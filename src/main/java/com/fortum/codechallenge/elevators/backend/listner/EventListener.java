7d5f6a3c8f99354a83092a961b50533db100ad3a
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-05 17:13:32 +0200

    EventListener created and beans creation enhanced

---
+++ src/main/java/com/fortum/codechallenge/elevators/backend/listner/EventListener.java
@@ -0,0 +1,9 @@
+package com.fortum.codechallenge.elevators.backend.listner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EventListener {
+}
