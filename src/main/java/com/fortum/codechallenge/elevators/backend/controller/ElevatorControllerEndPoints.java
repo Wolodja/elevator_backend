c42076ee0f23e118481cbe7d63778f6a62acb000
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-09 17:45:14 +0200

    Added CrossOrigin for elevators_frontend app

--- src/main/java/com/fortum/codechallenge/elevators/backend/controller/ElevatorControllerEndPoints.java
+++ src/main/java/com/fortum/codechallenge/elevators/backend/controller/ElevatorControllerEndPoints.java
@@ -9,6 +9,7 @@ import com.google.common.eventbus.EventBus;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import javax.validation.constraints.NotNull;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/rest/v1")
 public final class ElevatorControllerEndPoints {
 
@@ -57,3 +59,6 @@ public final class ElevatorControllerEndPoints {
     }
 
 }
+
+
+
